53b61218d2e3de71bd8b2022a138334967c2d4bf
Pietro Schgor <pfilippo0159@gmail.com>
2025-10-28 21:12:46 +0100

    aggiunto ggplot
    aggiunto ggplot per intero dataset e per paesi a scelta per capire meglio

--- dataManagement.R
+++ dataManagement.R
@@ -22,7 +22,54 @@ summary(poverty_rate)
 
 nCountries = length(unique(poverty_rate$Geopolitical.entity..reporting.))
 
-View(poverty_rate)
+#View(poverty_rate)
 
-color = rainbow(36)
-plot(x = poverty_rate$TIME_PERIOD, y = poverty_rate$OBS_VALUE)
+
+# PLOTS -------------------------------------------------------------------
+
+
+plot(poverty_rate$TIME_PERIOD, poverty_rate$OBS_VALUE)
+
+# install.packages("ggplot2")
+# install.packages("viridis") # Per la palette
+library(ggplot2)
+library(viridis)
+
+paese <- poverty_rate$Geopolitical.entity..reporting.
+
+ggplot(data = poverty_rate,
+       aes(x = TIME_PERIOD, y = OBS_VALUE, color = paese, group = paese)) + 
+  geom_line(linewidth = 1.5) +  # <- Linee Grosse (linewidth > 1)
+  scale_color_viridis(discrete = TRUE, option = "D") + # <- Palette Variata
+  geom_point(size = 3) + # Punti più grandi per abbinare le linee
+  labs(
+    title = "Tasso di Povertà per Paese nel Tempo",
+    x = "Periodo di Tempo",
+    y = "Tasso di Povertà"
+  )
+
+
+library(dplyr)
+
+# Definisci i paesi da includere
+paesi_selezionati <- c("France", "Italy", "Germany", "Bulgaria", "Poland")
+
+data_subsample <- poverty_rate %>%
+  # PASSO 1: Filtra il dataframe 
+  filter( poverty_rate$Geopolitical.entity..reporting. %in% paesi_selezionati) 
+
+
+paese <- data_subsample$Geopolitical.entity..reporting.
+  
+# PASSO 2: Crea il grafico con i dati filtrati
+ggplot( data = data_subsample,
+        aes(x = TIME_PERIOD, y = OBS_VALUE, color = paese, group = paese)) + 
+        geom_line(linewidth = 1.5) + # Linee grosse
+        geom_point(size = 3) +
+        scale_color_viridis(discrete = TRUE, option = "D") + 
+ 
+        labs(
+          title = "Tassi di Povertà: UK",
+          x = "Periodo di Tempo",
+          y = "Tasso di Povertà"
+        )
